88ad7364052ab57d702c24017419051faf707161
Olof2008 <olof.bergsten@elev.ga.ntig.se>
2026-02-12 09:35:12 +0100

    kryptering med nyckel 1

---
+++ index.py
@@ -0,0 +1,12 @@
+alabete = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z","å", "ä", "ö"]
+x = input("Skriv ett ord")
+ord = x
+
+
+for bokstav in ord:
+        if bokstav =="ö":
+            print(alabete[0])
+        else: 
+            plats = alabete.index(bokstav)
+            print(alabete[plats+1])
+      
